0189f141c9de15541df4aad45b9316e2a47165ce
Raksha Ashtankar <128995500+Raksha2122@users.noreply.github.com>
2023-11-08 12:30:33 +0530

    Add files via upload

---
+++ Speak_with_me.py
@@ -0,0 +1,17 @@
+#Install the library gtts
+#write on your command prompt py -m pip install gtts
+
+from gtts import gTTS
+import os
+
+user_input = input("Enter the text you want to convert to speech: ")
+text_to_speech = gTTS(user_input)
+
+# Save the speech to a file
+file_name = "Speak_with_me.mp3"
+text_to_speech.save(file_name)
+
+# Play the generated speech
+os.system(file_name)
+
+
